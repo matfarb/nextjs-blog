7801ea3051ae4461a8920fd866af465bd75bb29c
matfarb <mathew30@live.ca>
2022-04-27 14:08:17 -0400

    second commit

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi, I'm Mat.  I'm a developer that graduated from General Assembly's Software Engineering Immersive program.</p>
-        <p>
-          (This is a sample website - you'll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}Blog></h2>
